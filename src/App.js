b02e00989bf6b41c14432673718a87b396235d5f
tolik.siedin <tolik.siedin@flightright.de>
2024-12-19 11:01:05 +0200

    Refactoring

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import fullpage from 'fullpage.js';
-import './App.css';
-import 'fullpage.js/dist/fullpage.css';
-// import Typical from 'react-typical';
 import { TypeAnimation } from 'react-type-animation';
- 
+import 'fullpage.js/dist/fullpage.css';
+import './App.css';
+import slides from './data/slides';
+
 const CURSOR_CLASS_NAME = 'custom-type-animation-cursor';
 const EMPTY_CURSOR_CLASS_NAME = 'empty-custom-type-animation-cursor';
 
 const TypeTextComponent = (props) => {
   const { text } = props;
-  console.log(text);
   if (!text) return null;
   return (
     <TypeAnimation
@@ -19,7 +18,6 @@ const TypeTextComponent = (props) => {
       sequence={[
         text,
         3500,
-        // (el) => el.classList.remove(CURSOR_CLASS_NAME),
         (el) => el.classList.add(EMPTY_CURSOR_CLASS_NAME),
       ]}
       wrapper="p"
@@ -28,69 +26,6 @@ const TypeTextComponent = (props) => {
     />
   );
 };
-const slides = [
-  {
-    text: `
-      В неї очі янтарно смарагдові,
-      Її губи на смак як пуер,
-      Ці волосся, кудлаті, привабливі,
-      Що знаходжу я в ліжку тепер... `,
-    background: "url('/images/1.png')"
-  },
-  {
-    text: `
-      Ці дбайливі, ласкаві долоні,
-      Тонкі пальці, брови чорняві,
-      Мене ніжно тримають в полоні
-      В головній ролі у цій виставі.`,
-    background: "url('/images/qvi.gif')"
-  },
-  {
-    text: `
-      Поруч з нею я в космос злітаю,
-      Щоб зірки позбирати для нас,
-      Під ногами я землю втрачаю,
-      Та так міцно стою в одночас`,
-    background: "url('/images/3.png')"
-  },
-  {
-    text: `
-      Все навколо магічним чином
-      Без супротиву їй підкоряється.
-      З нею час по-інакшому плине,
-      Інші сенси в житті зʼявляються.`,
-    background: "url('/images/4.png')"
-  },
-  {
-    text: `
-      Коли я загублюсь у тиші,
-      Вона знайде мене наосліп,
-      Сяйвом сонячним заполонивши
-      Суматошний похмурий простір.`,
-    background: "url('/images/5.png')"
-  },
-  {
-    text: `
-      Врятувала, знайшла і зцілила
-      Серце вкрите глибокими шрамами,
-      Я його віддаю бережливо,
-      І пірнаю в волосся каштанове.`,
-    background: "url('/images/6.jpg')"
-  },
-  {
-    text: `
-      Ми зустрілись в цей час не даремно,
-      І позбувшись всіх зайвих тривог,
-      Проміж нами кликоче буремно
-      Це дихання - одне на двох`,
-    background: "url('/images/7.jpg')"
-  },
-  {
-    text: `
-       Київ, 19.12.2024.`,
-    background: "url('/images/2.png')"
-  },
-];
 
 function App() {
   const [activeSlide, setActiveSlide] = useState(0);
@@ -100,9 +35,9 @@ function App() {
       autoScrolling: true,
       licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
       onLeave: (origin, destination, direction) => {
+        // Control active slides to start typing animation
         setActiveSlide(destination.index);
       },
-
     });
   }, []);
 
